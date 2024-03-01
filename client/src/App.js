242bec12c84064a8b1db150bc607aa877e333699
Dhruv Patel <patel9d8@uwindsor.ca>
2024-02-29 21:30:32 -0500

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css'; // Import your CSS file
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CreateStudent from './components/createStudent/createStudent'
+
 
 import { useState } from 'react';
 import logImage from './img/log.svg';
